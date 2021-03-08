66369c1f70f60ee2d1f5ab20d3a55c5b5007def5
Filip <Artakaronja@gmail.com>
2021-03-08 14:26:21 +0100

    mere sjov

--- UDP Sjov/Client/client.cs
+++ UDP Sjov/Client/client.cs
@@ -4,28 +4,36 @@ using System.Net;
 using System.Net.Sockets;
 
 namespace client{
-class Client{
-static void Main(string[] args){
-    byte[] data = Encoding.ASCII.GetBytes("Hello World");
-    string ipAddress = "10.0.0.1";
-    int sendPort = 9000;
-    try
-    {
-        using (var client = new UdpClient())
-        {
-            Console.WriteLine("Created UdpClient");
-            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
-            Console.WriteLine("Created IpEndPoint");
-            client.Connect(ep);
-            Console.WriteLine("Connected?");
-            client.Send(data, data.Length);
-            Console.WriteLine("Data transmitted");
+    class Client{
+        static void Main(string[] args){
+             //derived from https://riptutorial.com/csharp/example/32222/basic-udp-client
+
+            byte[] data = Encoding.ASCII.GetBytes("u");
+            string ipAddress = "10.0.0.1";
+            int sendPort = 9000;
+            try
+            {
+                using (var client = new UdpClient())
+                {
+                    Console.WriteLine("Created UdpClient");
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                    Console.WriteLine("Created IpEndPoint");
+                    client.Connect(ep);
+                    Console.WriteLine("Connected?");
+                    client.Send(data, data.Length);
+                    Console.WriteLine("Data transmitted ep =" + ep.ToString());
+                    byte[] receivedData = client.Receive(ref ep);
+                    string recievedText=Encoding.ASCII.GetString(receivedData);
+
+                    Console.WriteLine(recievedText);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            
         }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.ToString());
-    }
-}
-}
 }
--- UDP Sjov/Server/server.cs
+++ UDP Sjov/Server/server.cs
@@ -2,25 +2,65 @@ using System;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace server{
     class Server{
         static void Main(string[] args){
+
+            //derived from https://riptutorial.com/csharp/example/32222/basic-udp-client
             bool done = false;
             int listenPort = 9000;
-            using(var listener = new UdpClient(listenPort))
+            using(UdpClient listener = new UdpClient(listenPort))
             {
+                Console.WriteLine("UDP client started");
                 IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+                
                 while(!done)
                 {
-                    byte[] receivedData = listener.Receive(ref listenPort);
+                    Console.WriteLine("Starting listening");
+                    byte[] receivedData = listener.Receive(ref listenEndPoint);
 
                     Console.WriteLine("Received broadcast message from client {0}", listenEndPoint.ToString());
 
                     Console.WriteLine("Decoded data is:");
-                    Console.WriteLine(Encoding.ASCII.GetString(receivedData)); //should be "Hello World" sent from above client
+                    string recievedText=Encoding.ASCII.GetString(receivedData);
+                    recievedText=recievedText.ToLower();
+                    Console.WriteLine(recievedText); //should be "Hello World" sent from above client
+                    
+                    string text;
+                    if(recievedText=="u") text = File.ReadAllText( "/proc/uptime" );//From https://stackoverflow.com/a/42110779
+                    else text ="Error";
+                    
+                    iptext=listenEndPoint.ToString();
+                    string[] arr=iptext.Split(":");
+                    sendData(text,new IPEndPoint(arr[0]+":9000"));
+                    
+                    
+
                 }
             }
-        }   
+        } 
+
+       static void sendData(string text, IPEndPoint ep){
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            try
+            {
+                using (var client = new UdpClient())
+                {
+                    Console.WriteLine("Created UdpClient for sending");
+                    client.Connect(ep);
+                    Console.WriteLine("Connected?");
+                    client.Send(data, data.Length);
+                    Console.WriteLine("Data transmitted");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            
+        }
     }
 }
